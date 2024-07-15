b7dc5ea3c6abcbe75248c70a091f9b35bb850dc2
AShwin <ashwin.thanga@gmail.com>
2024-07-15 13:54:18 +0530

    Understood response status codes and url redirecting

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require("http");
 const fs = require("fs");
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", (err, data) => {
+  path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.setHeader("Location", "/about");
+      res.statusCode = 301;
+      res.end();
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();

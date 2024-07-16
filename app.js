a42836914359cd43a7bb8995b84bd48dc16d7423
AShwin <ashwin.thanga@gmail.com>
2024-07-16 23:14:18 +0530

    use ejs package to create views for displaying dynamic data

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+//app.set("views", "myviews"); used to change the directory where it searches our ejs files
+
 app.get("/", (req, res) => {
-  //res.send("./views/about.html");
-  res.sendFile("./views/index.html", { root: __dirname });
+  //res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Home" });
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  //res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about",{title:"About"});
 });
 
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create",{title:"Create a Blog"});
 });
 
 //always use this in the end because express stops when a response is sent and does not continue with the remaining code
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  //res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404",{title:"404"});
 });
 
 app.listen(3000, (err) => {

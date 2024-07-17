eba02030638e7ecc66953e3bb1a4f28617c57dc7
AShwin <ashwin.thanga@gmail.com>
2024-07-17 23:55:06 +0530

    learnt to send dynamic data

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require("express");
 const app = express();
 
+const Blogs = [
+  {
+    title: "Day1",
+    snippet: "today I learnt about node js",
+  },
+  {
+    title: "Day2",
+    snippet: "today I implemented ejs ",
+  },
+];
+
 app.set("view engine", "ejs");
 //app.set("views", "myviews"); used to change the directory where it searches our ejs files
 
 app.get("/", (req, res) => {
   //res.sendFile("./views/index.html", { root: __dirname });
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Home", Blogs });
 });
 app.get("/about", (req, res) => {
   //res.sendFile("./views/about.html", { root: __dirname });
-  res.render("about",{title:"About"});
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create",{title:"Create a Blog"});
+  res.render("create", { title: "Create a Blog" });
 });
 
 //always use this in the end because express stops when a response is sent and does not continue with the remaining code
 app.use((req, res) => {
   //res.status(404).sendFile("./views/404.html", { root: __dirname });
-  res.status(404).render("404",{title:"404"});
+  res.status(404).render("404", { title: "404" });
 });
 
 app.listen(3000, (err) => {
